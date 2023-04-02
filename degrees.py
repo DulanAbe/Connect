c75fcf43ee5543047fd7b35a77c67cec05d00a72
Dulan Abeysundara <dulanabeysundara@Dulans-MacBook-Pro.local>
2023-04-02 20:15:04 +0100

    added files

---
+++ degrees.py
@@ -0,0 +1,184 @@
+# THIS IS THE ONE THAT WORKS PERFECTLY
+import csv
+import sys
+
+from util import Node, StackFrontier, QueueFrontier
+
+# Maps names to a set of corresponding person_ids
+names = {}
+
+# Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+people = {}
+
+# Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
+movies = {}
+
+
+def load_data(directory):
+    """
+    Load data from CSV files into memory.
+    """
+    # Load people
+    with open(f"{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set()
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
+
+    # Load movies
+    with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            movies[row["id"]] = {
+                "title": row["title"],
+                "year": row["year"],
+                "stars": set()
+            }
+
+    # Load stars
+    with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                people[row["person_id"]]["movies"].add(row["movie_id"])
+                movies[row["movie_id"]]["stars"].add(row["person_id"])
+            except KeyError:
+                pass
+
+
+def main():
+
+
+    # Load data from files into memory
+    print("Loading data...")
+    load_data("large")
+    print("Data loaded.")
+
+    source = person_id_for_name(input("Name: "))
+    if source is None:
+        sys.exit("Person not found.")
+    target = person_id_for_name(input("Name: "))
+    if target is None:
+        sys.exit("Person not found.")
+
+    path = shortest_path(source, target)
+
+    if path is None:
+        print("Not connected.")
+    else:
+        degrees = len(path)
+        print(f"{degrees} degrees of separation.")
+        path = [(None, source)] + path
+        for i in range(degrees):
+            person1 = people[path[i][1]]["name"]
+            person2 = people[path[i + 1][1]]["name"]
+            movie = movies[path[i + 1][0]]["title"]
+            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+
+
+def shortest_path(source, target):
+    """
+    Returns the shortest list of (movie_id, person_id) pairs
+    that connect the source to the target.
+
+    If no possible path, returns None.
+    """
+    # initialise frontier to just starting position IE input actor
+    # we will be working with ids
+    start = Node(source, None, None)
+    frontier = QueueFrontier()
+    frontier.add(start)
+
+    # initialise an empty explored set - have to do this ourselves, nothing in util.py
+    # will be an empty explored set of person_ids as they are the nodes
+    explored = set()
+
+    # begin the solution loop
+    while True:
+        # if frontier is empty, then we've reached the end of the dataset - no solution
+        if frontier.empty():
+            return(None)
+        # Remove a node from the frontier - queue frontier bc BFS.
+        node = frontier.remove()
+        # If node contains goal state return solution
+        # visualise as though you're at the solution and working backwards
+        if node.state == target:
+            actions = []
+            actions2 = []
+            while node.parent is not None:
+                actions.append((node.action, node.state))
+                actions2.append((id_to_movie(node.action), id_to_name(node.state)))
+                node=node.parent
+            actions.reverse()
+            return(actions)
+
+            # this must be a list of movies and stars, not too hard to append tbh
+            # this will be a whole process in and of itself
+        # else add node to explored set.
+        explored.add(node.state)
+        # expand node IE add neighbours to frontier
+        for movie_id, person_id in neighbors_for_person(node.state):
+            # print(f"Neighbour Contents: Actor: {id_to_name(person_id)} Movie: {id_to_movie(movie_id)}")
+            if not frontier.contains_state(person_id) and person_id not in explored:
+                child = Node(state = person_id, parent = node, action = movie_id)
+                frontier.add(child)
+
+
+def person_id_for_name(name):
+    """
+    Returns the IMDB id for a person's name,
+    resolving ambiguities as needed.
+    """
+    person_ids = list(names.get(name.lower(), set()))
+    if len(person_ids) == 0:
+        return None
+    elif len(person_ids) > 1:
+        print(f"Which '{name}'?")
+        for person_id in person_ids:
+            person = people[person_id]
+            name = person["name"]
+            birth = person["birth"]
+            print(f"ID: {person_id}, Name: {name}, Birth: {birth}")
+        try:
+            person_id = input("Intended Person ID: ")
+            if person_id in person_ids:
+                return person_id
+        except ValueError:
+            pass
+        return None
+    else:
+        return person_ids[0]
+
+def id_to_name(person_id):
+    for key, value in names.items():
+        if person_id in value:
+            return(key)
+
+def id_to_movie(movie_id):
+  for key, value in movies.items():
+    if movie_id == key:
+      return(value['title'])
+
+
+def neighbors_for_person(person_id):
+    """
+    Returns (movie_id, person_id) pairs for people
+    who starred with a given person.
+    """
+    movie_ids = people[person_id]["movies"]
+    neighbors = set()
+    for movie_id in movie_ids:
+        for person_id in movies[movie_id]["stars"]:
+            neighbors.add((movie_id, person_id))
+    return neighbors
+
+
+if __name__ == "__main__":
+    main()
